ab315a88fed8ccaecc9a3034458e1da4fefcbb5b
Hurel <jeremyhurel34@gmail.com>
2024-11-18 08:28:10 +0100

    wip

---
+++ src/agency.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+from connexion import FlaskApp
+
+app = FlaskApp(__name__)
+app.add_api(Path(__file__).parent / "../api/agency.yaml")  # noqa: F821
+
+
+def post_greeting(name: str):
+    return f"Hello {name}", 200
